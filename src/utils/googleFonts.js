52b72f85f056d9e0bf2327297a9a385830678a0e
Dallss <raalquicer@up.edu.ph>
2026-02-11 02:28:56 +0800

    hmm

--- src/utils/googleFonts.js
+++
@@ -1,9 +0,0 @@
-// add fonts here
-
-const fonts = ['Fjalla One', 'Roboto', 'Open Sans', 'Montserrat']
-
-const fontQuery = fonts.map((font) => `family=${font.replace(/ /g, '+')}`).join('&')
-
-const googleFontsURL = `https://fonts.googleapis.com/css2?${fontQuery}&display=swap`
-
-export default googleFontsURL
